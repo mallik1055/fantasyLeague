fa16e80449d762042510be0cdf3941e1e342a4a5
Mallikarjuna Rao <mallikarjuna@mysmartprice.com>
2020-02-14 04:48:24 -0500

    Update dbSettings.php

--- dbSettings.php
+++ dbSettings.php
@@ -1,4 +1,6 @@
 <?php
+
+
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
@@ -6,5 +8,9 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
+define("db_host",$server);
+define("db_user",$username);	
+define("db_password",$password);
+define("db_name",$db)
+  
 ?>
